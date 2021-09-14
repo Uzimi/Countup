a6599a89eae50e7ccf7f1e7259b7729cfbc913d8
Lumins <gridlinessc@gmail.com>
2021-09-14 16:46:06 +0000

    Log interactions to the console

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,22 @@
 const portle = require("../server");
 
 portle.on("ready", async (interaction) => {
+    const guildId = 884380331170484244n;
+
+    // This function will log all interactions to the console. Debugging purposes
+    const getApp = (guildId) => {
+        const app = portle.api.applications(portle.user.id)
+        if (guildId) {
+            app.guilds(guildId)
+        }
+        return app
+    }
+
+    // To delete slash commands use the method below
+    // await getApp(guildId).commands('887175174615609414').delete()
+
+    const slashcmds = await getApp(guildId).commands.get();
+    console.log('Interactions ->');
+    console.log(slashcmds);
     console.log("Portoise is wakey wakey");
 });
